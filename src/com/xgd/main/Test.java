21d8df0383785ed9edfb302e206eefe60928c959
xgd <58108709@qq.com>
2020-11-04 21:29:22 +0800

    修改了新建项目

--- src/com/xgd/main/Test.java
+++ src/com/xgd/main/Test.java
@@ -5,4 +5,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("徐国栋真帅");
     }
+
+    public void show1(){
+        System.out.println("徐国栋你真棒");
+    }
 }
